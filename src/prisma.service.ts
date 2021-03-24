4981b87098a81c98aac08ede193a0b002ce8694c
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-03-24 16:14:35 +0100

    feat(logging): make console logging work

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,15 +1,30 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
+  extends PrismaClient<{ log: [{ emit: 'event'; level: 'query' }] }>
   implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
   async onModuleInit() {
-    await this.$connect()
+    this.$on('query', (e) => {
+      console.log('Query: ' + e.query);
+      console.log('Duration: ' + e.duration + 'ms');
+    });
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect()
+    await this.$disconnect();
   }
 }
